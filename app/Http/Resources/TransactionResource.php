ab4ca305cd1447f784f4caeae2c21f146c77db11
eddimull <eddimull@gmail.com>
2022-07-13 15:54:08 -0500

    first commit

---
+++ app/Http/Resources/TransactionResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\CategoryResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'description' => $this->description,
+            'amount' => number_format($this->amount/100, 2),
+            'transaction_date' => $this->transaction_date->format('m/d/Y'),
+            'category_id' => $this->category_id,
+            'category_name'=> $this->category->name,
+            'created_at' => $this->created_at->format('m/d/Y'),
+        ];
+    }
+}
